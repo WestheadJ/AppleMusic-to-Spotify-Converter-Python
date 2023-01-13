c97f88790a691f1dbc0079a0e7d9457c23313398
James Westhead <james_westhead@outlook.com>
2023-01-13 04:06:05 +0000

    Added main.py (needs comments) - Needs a readme

--- appToSpot/main.py
+++ appToSpot/main.py
@@ -1,30 +1,76 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from configs import CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,SCOPE,username
+import datetime
 
 # Making a GET request
-r = requests.get('https://music.apple.com/gb/playlist/untitled-playlist/pl.u-9N9LX8yI1amWkjA')
+try:
+    url = input("Enter Apple Music playlist URL: ")
+    r = requests.get(url)
 
-# Parsing the HTML
-soup = BeautifulSoup(r.content, 'html.parser')
+except:
+    raise Exception("Error URL is incorrect")
 
-trackTitleHTML = soup.findAll('div', class_='songs-list-row__song-name')
-trackArtistsHTML = soup.findAll('div', class_="songs-list-row__by-line svelte-1yo4jst")
-trackArtistItems = []
-tracks = []
+if(r.status_code == 404):
+    raise Exception("URL is incorrect, please check if URL is correct")
+else:
+    # Parsing the HTML
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-for item in trackArtistsHTML:
-    print(item.find_all('a'))
-    for artists in item.find_all(('a')):
-        print(artists)
-    # trackArtistItems.append(item.find_all('a').text)
+    trackTitleHTML = soup.findAll('div', class_='songs-list-row__song-name')
+    trackArtistsHTML = soup.findAll('div', class_="songs-list-row__by-line svelte-1yo4jst")
+    trackArtistItems = []
+    tracks = []
+    songsToAdd = []
 
-print(trackArtistItems)
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI,scope=SCOPE))
 
-# count = 0
-# for i in trackTitleHTML:
-#     tracks.append([i.text,0])
-#
-# count = 0
-# for item in trackArtistsHTML:
-#     trackArtistItems.append(item.find_all('a'))
+    playlist_name = input("Enter a playlist name: ")
+    playlist_description = input("Enter a playlist description: ")
+    playlist_privacy = input("Playlist Privacy Public/Private (default is private just leave empty):")
+    playlistPublicy = False
+    if (playlist_privacy.lower() == "public"):
+        playlistPublicy = True
+    elif (playlist_privacy.lower() == "private"):
+        pass
+    elif (playlist_privacy.lower() == ""):
+        pass
+    else:
+        raise Exception("Needs to be public or private you entered", playlist_privacy)
 
+    sp.user_playlist_create(user=username, name=playlist_name, public=True, description=playlist_description)
+
+    for item in trackArtistsHTML:
+        trackArtistItems.append(item.find_all('a'))
+
+    for i in trackTitleHTML:
+        tracks.append(i.string)
+
+    count = 1
+    for i in tracks:
+        count += 1
+
+    count = 1
+    for item in trackArtistItems:
+        songsToAdd.append([item[0].string, tracks[count - 1]])
+        count += 1
+
+    tracksQuery = []
+    notFound = []
+    for track in songsToAdd:
+        result = sp.search(q="artist:" + track[0] + " track:" + track[1], type="track")
+        if (len(result['tracks']['items']) != 0):
+            tracksQuery.append(result['tracks']['items'][0]['uri'])
+        else:
+            print("Could not find song", track[1], "by", track[0])
+            notFound.append("Could not find song " + track[1] + " by " + track[0])
+    out_file = open(str(datetime.datetime.now().strftime("%a-%d-%b-%Y_%H-%M-%S-%f"))+".txt", "w")
+    for missedTrack in notFound:
+        out_file.writelines(missedTrack+"\n")
+    out_file.close()
+    getRecentPlaylist = sp.user_playlists("lt3wbutw7ie0cz0u1mrei63e2")
+    playlistID = getRecentPlaylist['items'][0]['id']
+
+    sp.user_playlist_add_tracks(user=username, playlist_id=playlistID, tracks=tracksQuery)
