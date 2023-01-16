4dd7ed0e9ff7b7e340f4b277cc014b26e4828b23
isaac238 <isaac@isaac238.dev>
2023-01-16 02:02:34 +0000

    fix: fixed issues #2 #3 #4 #5 #6 see pull req for details.

--- colours.py
+++ colours.py
@@ -2,3 +2,7 @@ class color:
     KEY = '\033[1;34;48m'
     SUCCESS = '\033[1;32;48m'
     ERROR = '\033[1;31;48m'
+    RESET = '\u001b[0m'
+
+def colorMessage(inputColor, message):
+    return f"{inputColor} {message} {color.RESET}"
